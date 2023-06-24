dbf59187177d2bf44e479378643f768e7af5e161
diggilly <126244085+diggilly@users.noreply.github.com>
2023-06-24 16:50:34 +0300

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -2,9 +2,13 @@
 export async function all(m) {
 	
     // when someone sends a group link to the bot's dm
-    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('use this link to join my whatsApp group') || m.text.startsWith('hello can i add you to my group') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
      this.sendButton(m.chat, `*Invite bot to a group* 
-        
+
+// when someone sends a group link to the bot's dm
+    if ((m.mtype === 'greeting message' || m.text.startsWith('hello') || m.text.startsWith('hey') || m.text.startsWith('hello can i add you to my group') || m.text.startsWith('are you there') || m.text.startsWith('hallo')) && !m.isBaileys && !m.isGroup) {
+     this.sendButton(m.chat, `*I am here how can i e of any help type .list to view my commands* 
+ 
   Hallo @${m.sender.split('@')[0]} 
   you can rent the bot to join a group or contact owner 
   more info click on the button
