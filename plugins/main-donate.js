f1a68c826ca3cf15374fd67e7d65f01e527d4eb6
diggilly <126244085+diggilly@users.noreply.github.com>
2023-06-27 11:52:20 +0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,9 +3,10 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *DONATION*
-ONLY WANT YOUR LOVE
-SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
-let img = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
+ONLY WANT YOUR LOVE 
+Join my Whatsapp group => https://chat.whatsapp.com/KggVmnH7ki6HoyD0025gDD
+SUPPORT ME BY  MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
+let img = 'https://raw.githubusercontent.com/diggilly/kinflux-bot/main/Guru.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
